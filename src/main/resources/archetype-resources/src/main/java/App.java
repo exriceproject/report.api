a4619267e274f388f189c07dccab83bb6f03ae7a
Tawatchai Radom <tawatchr0009@gmail.com>
2018-11-04 16:42:23 +0700

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.exrice.erp.api.report;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
